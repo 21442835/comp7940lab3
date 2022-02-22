0638cce65be6a629679567a3de119ce5c2750188
21442835@life.hkbu.edu.hk <21442835@life.hkbu.edu.hk>
2022-02-22 21:19:31 +0800

    'chatbotlab5Again'

--- chatbot.py
+++ chatbot.py
@@ -20,7 +20,7 @@ def main():
 
 
     global redis1
-    redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']), port=(config['REDIS']['REDISPORT']))
+    redis1 = redis.Redis(host=(os.environ['HOST']), password=(os.environ['PASSWORD']), port=(os.environ['REDISPORT']))
 
     # You can set this logging module, so you will know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
